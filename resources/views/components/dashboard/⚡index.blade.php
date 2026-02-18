36194b06ef6153177b8de6b04a4be685d46ce9e6
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2026-02-18 16:33:23 -0300

    Refresh buttons added

--- resources/views/components/dashboard/⚡index.blade.php
+++ resources/views/components/dashboard/⚡index.blade.php
@@ -24,22 +24,22 @@ new class extends Component
 ?>
 <div>
   {{-- <x-refresh wire:click="$refresh" wire:target="$refresh" wire:island="stats" /> --}}
-  <button wire:click="$refresh" wire:target="$refresh" wire:island="stats" class="bg-indigo-600 text-white p-1 rounded mb-1 cursor-pointer">Load</button>
+  {{-- <button wire:click="$refresh" wire:target="$refresh" wire:island="stats" class="bg-indigo-600 text-white p-1 rounded mb-1 cursor-pointer">Load</button> --}}
   <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-4">
-    @island(lazy:true, name:'stats')
+    @island(lazy:true)
     @placeholder
     <x-dashboard.loading-card />
     @endplaceholder
     <x-dashboard.card :data="$this->sales" text="Sales" subtext="Sales of the month">
-      {{-- <x-refresh wire:click="$refresh" wire:target="$refresh" /> --}}
+      <x-refresh wire:click="$refresh" wire:target="$refresh" />
     </x-dashboard.card>
     @endisland
-    @island(lazy:true, name:'stats')
+    @island(lazy:true)
     @placeholder
     <x-dashboard.loading-card />
     @endplaceholder
     <x-dashboard.card :data="$this->users" text="Users" subtext="Users registered">
-      {{-- <x-refresh wire:click="$refresh" wire:target="$refresh" /> --}}
+      <x-refresh wire:click="$refresh" wire:target="$refresh" />
     </x-dashboard.card>
     @endisland
   </div>

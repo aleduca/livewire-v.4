c1904b8a1d5b56e414175057f2f7b5f26b21e853
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2026-02-18 09:08:25 -0300

    Islands

--- resources/views/components/dashboard/⚡index.blade.php
+++ resources/views/components/dashboard/⚡index.blade.php
@@ -1,22 +1,33 @@
 <?php
 
+use App\Models\User;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\Reactive;
 use Livewire\Component;
 
 new class extends Component
 {
-  public $users = 0;
+  // #[Reactive]
+  // public $users;
 
   #[Computed]
   public function sales(){
     return Number::currency(mt_rand(1000,5000), 'BRL', 'pt_BR');
   }
 
+   #[Computed]
+  public function users(){
+    return User::count();
+  }
+
 };
 ?>
 
 <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-4">
+  @island
   <x-dashboard.card :data="$this->sales" text="Sales" subtext="Sales of the month" />
-  <x-dashboard.card :data="$this->users" text="Users" subtext="Users registered" />
+  @endisland
+  @island
+  <x-dashboard.card wire:poll.5s :data="$this->users" text="Users" subtext="Users registered" />
+  @endisland
 </div>
